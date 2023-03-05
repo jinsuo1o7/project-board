117a9347bc08749ee15c14612c6f056083fcfa76
Jinsuo <cil04110@gmail.com>
2023-03-06 04:04:57 +0900

    #12 - db
    change project file system, Modifications not related to branches

--- src/main/java/com/project/board/domain/Article.java
+++ src/main/java/com/project/board/domain/Article.java
@@ -1,8 +1,18 @@
 package com.project.board.domain;
 
 import com.project.board.domain.base.BaseTimeEntity;
+import lombok.Getter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+//@Table(indexes = )
 public class Article extends BaseTimeEntity {
+    @Id @GeneratedValue
     private Long id;
     private String title;
     private String content;
