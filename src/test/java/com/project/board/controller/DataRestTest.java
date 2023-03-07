dbad0d2fccc700f7f1b83f661c84f085db0bd3ce
Jinsuo <cil04110@gmail.com>
2023-03-07 20:32:11 +0900

    #7 - Querydsl dependencies
    query dsl dependencies setting and apply jpa repository

--- src/test/java/com/project/board/controller/DataRestTest.java
+++ src/test/java/com/project/board/controller/DataRestTest.java
@@ -5,15 +5,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Disabled("String data rest 통합테스트 제외")
