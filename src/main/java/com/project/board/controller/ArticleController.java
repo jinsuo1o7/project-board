94c15e220f91ffa85f859d2d281967bf825d9ba1
Jinsuo <cil04110@gmail.com>
2023-03-07 22:16:22 +0900

    #18 - board article view test
    Temporarily disable because gradle build does not work in mvc test by writing only test

---
+++ src/main/java/com/project/board/controller/ArticleController.java
@@ -0,0 +1,11 @@
+package com.project.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/article")
+public class ArticleController {
+}
---
+++ src/test/java/com/project/board/controller/ArticleControllerTest.java
@@ -0,0 +1,67 @@
+package com.project.board.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("viw controller - article")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Disabled("TODO task")
+    @DisplayName("[view][GET] article list page")
+    @Test
+    void givenNothing_whenRequestingArticlesView_thenArticlesView() throws Exception {
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @Disabled("TODO task")
+    @DisplayName("[view][GET] article detail page")
+    @Test
+    void givenNothing_whenRequestingArticleDetailView_thenArticleDetailView() throws Exception {
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
+    }
+    @Disabled("TODO task")
+    @DisplayName("[view][GET] article search page")
+    @Test
+    void givenNothing_whenRequestingArticleSearchView_thenArticleSearchView() throws Exception {
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("articles/search"))
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+
+    @Disabled("TODO task")
+    @DisplayName("[view][GET] article hash tag search page")
+    @Test
+    void givenNothing_whenRequestingArticleHashTagSearchView_thenArticleHashTagSearchView() throws Exception {
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(view().name("articles/hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+
+}
